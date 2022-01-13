41ff70a2d89aaa0b1024b7ed1ea4bb6e78fca470
Anshuman Bisoyi <ansuman746@gmail.com>
2022-01-14 00:18:01 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Supublic static void main(String[] args) {
+    {
+        public static void main(String Args[])
+        {
+            int a=7
+            int h=8
+            System.out.println("The product is: "+(a*h);
+        }
+    }
+}
